5061a291b93763fc7f14f37e27088ab21d7696b7
hrkn63-hnm <hanamu.mado@gmail.com>
2024-05-28 05:03:00 +0000

    finished step2

--- hello_world/settings.py
+++ hello_world/settings.py
@@ -37,6 +37,7 @@ if 'CODESPACE_NAME' in os.environ:
 # Application definition
 
 INSTALLED_APPS = [
+    "polls.apps.PollsConfig",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
